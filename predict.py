7b48081e8a8c5348158b8405fe85c8e6535667c2
Mbugz1000 <44344898+Mbugz1000@users.noreply.github.com>
2022-05-18 10:16:12 +0300

    Updated all the methods with comments

--- predict.py
+++ predict.py
@@ -16,6 +16,10 @@ IMAGE_SIZE = 224
 
 
 def load_model():
+    """
+    This function loads the model as well as the pre-trained model for prediction
+    :return: Keras model
+    """
     dir_ = "data/for_modelling/tf2-preview_mobilenet_v2_feature_vector_4"
     feature_extractor = hub.KerasLayer(dir_, input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
     feature_extractor.trainable = False
@@ -27,6 +31,10 @@ def load_model():
 
 
 def load_class_names():
+    """
+
+    :return: Dictionary that maps class labels to class names
+    """
     with open('label_map.json', 'r') as f:
         class_names = json.load(f)
 
@@ -34,6 +42,12 @@ def load_class_names():
 
 
 def process_image(image_numpy_array):
+    """
+    This function resizes and normalizes the images
+
+    :param image_numpy_array: Image in form of a numpy array
+    :return: Transformed image tensor
+    """
     image_tensor = tf.convert_to_tensor(image_numpy_array)
     image_tensor = tf.image.resize(image_tensor, size=(IMAGE_SIZE, IMAGE_SIZE))
     image_tensor /= 225
@@ -42,6 +56,14 @@ def process_image(image_numpy_array):
 
 
 def predict(image_path, model, top_k):
+    """
+    This method predicts the flower that's in a given image.
+
+    :param image_path: The path to the input image
+    :param model: Trained model
+    :param top_k: The top k most probable flowers in the list
+    :return: Probabilities of the top k flowers and Labels of the top k flowers
+    """
     im = Image.open(image_path)
     test_image = np.asarray(im)
 
@@ -54,6 +76,16 @@ def predict(image_path, model, top_k):
 
 
 def main(image_path, top_k):
+    """
+    The main method of this command line app. It performs the following tasks:
+        1. Loads the model,
+        2. Loads the class names
+        3. Performs the prediction the returns the output to the user
+
+    :param image_path: The path to the input image
+    :param top_k: The top k most probable flowers in the list
+    :return: None
+    """
     model = load_model()
     class_names = load_class_names()
 
@@ -62,7 +94,9 @@ def main(image_path, top_k):
     print(f"The predicted image is a {class_names[classes[-1]]}. Predicted probability is {ps[-1]:.4f}\n")
 
     next_likely_flowers = list(map(class_names.get, classes[:-1]))
-    print(f"The next {top_k-1} likely flowers according to the model are {', '.join(next_likely_flowers)}")
+    print(f"The next {top_k-1} likely flowers according to the model are:")
+    for i, (flower, prob) in enumerate(zip(next_likely_flowers[::-1], ps[::-1][1:]), start=2):
+        print(f"{i}. {flower}. Prob: {prob:.4f}")
 
 
 if __name__ == "__main__":
